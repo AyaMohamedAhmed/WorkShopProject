2b95c49f859aa53e8f733aeb7a303127562fc5a6
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 <Hudakamal6>
2023-05-26 19:08:48 +0300

    Fav Screen UI

--- WorkShopProject/Moduals/Favourite/View/FavoriteViewController.swift
+++ WorkShopProject/Moduals/Favourite/View/FavoriteViewController.swift
@@ -9,12 +9,40 @@ import UIKit
 
 class FavoriteViewController: UIViewController {
 
+    @IBOutlet weak var favTable: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+       activeView()
     }
     
 
+    func activeView(){
+        favTable.delegate = self
+        favTable.dataSource = self
+        
+        favTable.register(UINib(nibName: "FavoriteCell", bundle: nil), forCellReuseIdentifier: "FavoriteCell")
+    }
 
+}
+
+extension FavoriteViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FavoriteCell", for: indexPath) as! FavoriteCell
+
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
+    
+    
+    
 }
